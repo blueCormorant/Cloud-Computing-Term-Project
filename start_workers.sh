0a913ae2866d428c4fca4172ad104e3046431271
blueCormorant <lucavdelsignore@gmail.com>
2023-05-12 16:34:05 -0400

    Change workers to single process

--- start_workers.sh
+++ start_workers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-celery -A celery_worker worker -E --loglevel=INFO -n worker1 -P eventlet -c 8 &
-celery -A celery_worker worker -E --loglevel=INFO -n worker2 -P eventlet -c 8 &
-celery -A celery_worker worker -E --loglevel=INFO -n worker3 -P eventlet -c 8 &
+celery -A celery_worker worker -E --loglevel=INFO -n worker1 -P eventlet -c 1 &
+celery -A celery_worker worker -E --loglevel=INFO -n worker2 -P eventlet -c 1 &
+celery -A celery_worker worker -E --loglevel=INFO -n worker3 -P eventlet -c 1 &
