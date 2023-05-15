3ff238b3d720173fc5273f9dc999a62596b5e595
blueCormorant <lucavdelsignore@gmail.com>
2023-05-15 04:24:14 -0400

    Preprocess and postprocess file uploads

--- app.py
+++ app.py
@@ -6,10 +6,55 @@ from tasks import translate_file
 from tasks import tokenizer, model
 from tasks import Response
 import json
+import nltk
+
+nltk.download('punkt')
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = 'your_secret_key_here'
+
+
+def chunk_text(text, max_tokens=50):
+    tokens = nltk.word_tokenize(text)
+    chunks = []
+
+    current_chunk = []
+    current_chunk_tokens = 0
+
+    for token in tokens:
+        if current_chunk_tokens + len(nltk.word_tokenize(token)) <= max_tokens:
+            current_chunk.append(token)
+            current_chunk_tokens += len(nltk.word_tokenize(token))
+        else:
+            chunks.append(' '.join(current_chunk))
+            current_chunk = [token]
+            current_chunk_tokens = len(nltk.word_tokenize(token))
+
+    chunks.append(' '.join(current_chunk))  # Add the last chunk
+
+    return chunks
+
+def postprocess_jobs(jobs):
+    responses = sorted(jobs, key=lambda x: (x['filename'], x['index']))
+
+    grouped_dict = {}
+    for response in responses:
+        try:
+            grouped_dict[response["filename"]].append(response["text"])
+        except KeyError:
+            grouped_dict[response["filename"]] = []
+            grouped_dict[response["filename"]].append(response["text"])
+    
+    grouped_list = []
+    for filename in grouped_dict:
+        file_dict = {}
+        file_dict["filename"] = filename
+        file_dict["text"] = " ".join(grouped_dict[filename])
+        grouped_list.append(file_dict)
+
+    return grouped_list
+
 
 @app.route('/')
 def index():
@@ -23,7 +68,8 @@ def upload_file():
     for file in files:
         text = str(file.read().decode('utf-8'))
         filename = file.filename
-        tasks.append(translate_file.subtask((filename, text)))
+        for index, chunk in enumerate(chunk_text(text)):
+            tasks.append(translate_file.subtask((filename, index, chunk)))
 
     # use a Celery group to run all tasks in parallel
     job = group(tasks).apply_async()
@@ -32,7 +78,8 @@ def upload_file():
     job.join()
 
     # get the results of each task
-    responses = job.get()
+    responses = postprocess_jobs(job.get())
+
     return render_template('index.html', responses=responses)
 
 @app.route('/login', methods=["GET"])
